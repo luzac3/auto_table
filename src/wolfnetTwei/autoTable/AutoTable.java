c196e3f29d2795ea1b2a178b0c9fd086ebc82bbe
luzac3 <rey.silvy@gmail.com>
2018-05-08 06:30:12 +0900

    Ant追加、ディレクトリ整理、ログの追加

--- src/wolfnetTwei/autoTable/AutoTable.java
+++ src/wolfnetTwei/autoTable/AutoTable.java
@@ -18,10 +18,14 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 
 /**
  *
- * 引数で取得したディレクトリのCSVファイル一覧を取得<br>
+ * 引数で取得したディレクトリのCSVかXLSファイル一覧を取得<br>
  * ファイル(テーブル)ごとにリストを作成する関数を呼び出す<br>
  * 取得したマップをファイルSQLファイルを生成する関数に渡す<br>
  *
@@ -30,33 +34,44 @@ import java.util.Map;
  *
  */
 class AutoTable{
+    /** Logger SLF4J */
+    private static Logger log = Logger.getLogger(AutoTable.class);
+
     public static void main(String args[]){
-        String fileLstPath = "";
+        DOMConfigurator.configure("./log4j.xml");
+
+        final String FILE_LST_PATH = "..\\file";
         String filePath = "";
 
         Map<String, List<Object>> tblMap = new HashMap<>();
 
-        if(args.length != 0){
-            fileLstPath = args[0];
             File fils = null;
             String csvLst[] = null;
 
             try{
+                log.trace("ファイル取得開始");
                 // ファイルの取得
-                fils = new File(fileLstPath);
+                fils = new File(FILE_LST_PATH);
 
                 // CSVファイル一覧のリスト
                 csvLst = fils.list();
             }catch(SecurityException e){
+                log.error("アクセス拒否されました：" + e);
                 System.out.println("アクセス拒否されました");
             }catch(Exception e){
                 System.out.println("その他のエラー");
+                log.error("未知のエラーです：" + e);
                 throw new RuntimeException(e);
             }
 
+            log.info("ファイル読み込み開始");
+            if(Objects.isNull(csvLst)){
+                log.info("ファイルがありません");
+                return;
+            }
             for (String csvName : csvLst){
                 // マップに追加するCSVファイルのフルパス
-                filePath = fileLstPath + "\\" + csvName;
+                filePath = FILE_LST_PATH + "\\" + csvName;
 
                 FileLoader fileLoader = new FileLoader();
 
@@ -65,21 +80,23 @@ class AutoTable{
                     tblMap = fileLoader.fileLoad(filePath,tblMap);
                 }catch(ExtentionException e){
                     // この例外の場合は該当ファイルをスキップ
+                    log.info("ファイルをスキップしました");
                     continue;
                 }catch(IOException e){
+                    log.error(e);
                     throw new RuntimeException(e);
                 }catch(Exception e){
-                	throw new RuntimeException("未知のエラーです");
+                    log.error("未知のエラーです：" + e);
+                    throw new RuntimeException("未知のエラーです");
                 }
+                log.info("ファイル読み込み終了");
             }
-        }else{
-            // 引数が取れてないのでエラー
-        	throw new RuntimeException("引数がありません");
-        }
 
         SqlMaker sqlMaker = new SqlMaker();
 
+        log.info("ファイル書き込み開始");
         // 作成したマップを受け取り、ファイル生成する関数の呼び出し
         sqlMaker.makeStr(tblMap);
+        log.info("ファイル書き込み完了");
     }
 }
